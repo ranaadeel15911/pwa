7ec02e6c2347f0fba27ab6a341134130154f3a32
ranaadeel15911 <ra0511083@gmail.com>
2025-02-07 22:57:28 +0500

    .

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import InstallPWA from './InstallPWA';
 import BahrainButton from './Components/BahrainButton';
-
+import { useEffect, useState } from 'react';
+import useGeoLocation from "react-ipgeolocation";
 function App() {
+  const location = useGeoLocation();
+console.log(location.country);
+console.log(location)
+  // const [country, setCountry] = useState("");
+  // useEffect(() => {
+  //   fetch("https://geolocation-db.com/json/")
+  //     .then((res) => res.json())
+  //     .then((data) => setCountry(data.country_name))
+  //     .catch((err) => console.error("Error fetching location:", err));
+  // }, []);
+  // console.log(country)
   return (
     <div className="App">
-    <h1>Welcome to My PWA</h1>
-    <p>Enjoy a fast, offline experience.</p>
+    {/* <h1>Welcome to My PWA</h1>
+    <p>Enjoy a fast, offline experience.</p> */}
+    <p>
+Here is Your Country Code <h1>location</h1>
+    </p>
     {/* <InstallPWA /> */}
-    <BahrainButton />
+    {/* <BahrainButton /> */}
   </div>
   );
 }
